3988247f9398cf84c00f8a695b65341cca5b3c17
Royce Pipkins <rpipkins@thinkliquidity.com>
2019-02-02 11:32:51 -0600

    Adapted for TTGO board

--- WiFiManager.ino
+++ WiFiManager.ino
@@ -1,9 +1,14 @@
 #include <WiFi.h>
 #include <DNSServer.h>
-#include <ESP8266WebServer.h>
+#include <WebServer.h>
 #include <WiFiManager.h>         
-#include <M5Stack.h>
 #include <vector>
+#include <sstream>
+#include <iomanip>
+#include <Adafruit_GFX.h>                                  // Core graphics library
+#include <Adafruit_ST7735.h>                               // Hardware-specific library
+Adafruit_ST7735 tft = Adafruit_ST7735(16, 17, 23, 5, 9); // CS,A0,SDA,SCK,RESET
+
 
 using namespace std;
 
@@ -13,14 +18,14 @@ using namespace std;
 template <typename ArgType> void MirrorPrint(ArgType arg)
 {
   Serial.print(arg);
-  M5.Lcd.print(arg);
+  tft.print(arg);
 }
 
 
 template <typename ArgType> void MirrorPrintln(ArgType arg)
 {
   Serial.println(arg);
-  M5.Lcd.println(arg);
+  tft.println(arg);
 }
 
 
@@ -30,8 +35,9 @@ void eraseConfig()
 {
   //Note: WiFi and SPIFFS must have already been started
   WiFi.disconnect(true); 
-  M5.Lcd.fillScreen(TFT_GREY);
-  M5.Lcd.setCursor(0, 0, 2);
+  tft.fillScreen(TFT_GREY);
+  tft.setCursor(0, 0);
+  tft.setTextSize(2);
   MirrorPrintln("Erased Wifi info");
   while(1) delay(1000);
 }
@@ -42,26 +48,34 @@ void eraseConfig()
 
 void setup() {
   Serial.begin(115200);
-  M5.begin();
+
+  tft.initR(INITR_18GREENTAB);                             // 1.44 v2.1
+  tft.fillScreen(ST7735_BLACK);                            // CLEAR
+  tft.setTextColor(0x5FCC);                                // GREEN
+  tft.setRotation(1);                                      // 
 
   //WiFiManager
   //Local intialization. Once its business is done, there is no need to keep it around
   WiFiManager wifiManager;
 
-  M5.Lcd.fillScreen(TFT_GREY);
-  M5.Lcd.setCursor(0, 0, 2);
-  M5.Lcd.setTextColor(TFT_WHITE,TFT_BLACK);  
-  M5.Lcd.setTextSize(1);
+  //eraseConfig();
+
+  
 
   MirrorPrintln("If device is not online in a few seconds");
   MirrorPrintln("then connect to SSID: ");
-  MirrorPrintln("ESP" + String(ESP_getChipId()));
- 
+
+  stringstream as;
+  as << "ESP" << hex << ESP.getEfuseMac();
+  
+  String auto_ssid(as.str().c_str());
+  MirrorPrintln(auto_ssid);
   //fetches ssid and pass and tries to connect
   //if it does not connect it starts an access point 
   //named after the chip ID
   //and goes into a blocking loop awaiting configuration
-  if (!wifiManager.autoConnect()) {
+  wifiManager.setConnectTimeout(6);
+  if (!wifiManager.autoConnect(auto_ssid.c_str())) {
     MirrorPrintln("failed to connect and hit timeout");
     delay(3000);
     //reset and try again, or maybe put it to deep sleep
@@ -75,8 +89,7 @@ void setup() {
 
 void loop() {
 
-  M5.Lcd.fillScreen(TFT_GREY);
-  M5.Lcd.setCursor(0, 0, 2);
+  
   MirrorPrint("SSID: ");
   MirrorPrintln(WiFi.SSID());
   MirrorPrint("IP: ");
